9c0fb5c3e8f17369e17d9dfe52c16ce0f3ef51cc
matty uni laptop <mattylee0121@gmail.com>
2024-10-25 16:21:21 +0100

    started working on test.py will rename for sin cos tan graph transformations

---
+++ Uni Assignments/Section_A/extra/test.py
@@ -0,0 +1,12 @@
+#automate sin cos tan graph transformations
+outside = input("what is outside the bracket?")
+inside = input("what is inside the bracket?")
+
+
+
+def insidebracket(xCoef, xaddition):
+    #inside the bracket opposite
+    
+    xaddition = xaddition * -1
+    xCoef = 1 / xCoef
+
