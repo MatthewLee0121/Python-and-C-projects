d4e5d015dc34f58aae480495209480e43510cf28
matty uni laptop <mattylee0121@gmail.com>
2024-10-25 16:25:56 +0100

    explains itself

--- Uni Assignments/Section_A/extra/test.py
+++ Uni Assignments/Section_A/extra/test.py
@@ -1,12 +1,15 @@
 #automate sin cos tan graph transformations
-outside = input("what is outside the bracket?")
-inside = input("what is inside the bracket?")
 
-
-
-def insidebracket(xCoef, xaddition):
+def insidebracket():
     #inside the bracket opposite
-    
+    xCoef = int(input("what is the coefficient of x? "))
+    xaddition = int(input("what is the amount added or subtracted? remember to include the sign "))
     xaddition = xaddition * -1
     xCoef = 1 / xCoef
 
+insidebracket()
+
+def indetifytrigfunc():
+    trigfunc = input("sin, cos or tan? ")
+
+    return trigfunc
