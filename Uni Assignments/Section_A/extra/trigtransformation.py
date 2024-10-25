c595010767f34604cadb10e1852c34f5e2ea9c0e
matty uni laptop <mattylee0121@gmail.com>
2024-10-25 16:42:52 +0100

    more work on trig trans skeleton

--- Uni Assignments/Section_A/extra/trigtransformation.py
+++ Uni Assignments/Section_A/extra/trigtransformation.py
@@ -1,15 +1,26 @@
 #automate sin cos tan graph transformations
 
 def insidebracket():
-    #inside the bracket opposite
-    xCoef = int(input("what is the coefficient of x? "))
-    xAddition = int(input("what is the amount added or subtracted? remember to include the sign "))
+    #grabbing values
+    xCoef = int(input("what is the coefficient of x inside the bracket? "))
+    xAddition = int(input("what is the amount added inside the bracket? "))
     xAddition = xAddition * -1
     xCoef = 1 / xCoef
 
+    return xCoef, xAddition
+
+def outsidebracket():
+    #grabbing values
+    outCoef = int(input("what value are you multiplying by outside the bracket? "))
+    outAddition = int(input("what value are you adding by outside the bracket? "))
+
+    return outCoef, outAddition
+
 def identifytrigfunc():
+    #are we dealing with sin cos tan?
     trigfunc = input("sin, cos or tan? ")
 
     return trigfunc
 
-print(insidebracket(), identifytrigfunc())
+equationvalues = [insidebracket(), outsidebracket(), identifytrigfunc()]
+print(equationvalues)
