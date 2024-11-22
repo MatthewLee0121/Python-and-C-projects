b7aa8bfe7a72feb0a683c769da6e99fd13413b98
Matty Laptop <mattylee0121@gmail.com>
2024-11-22 12:35:43 +0000

    some changes made to assignment 3

--- Uni Assignments/Section_A/assignment3.py
+++ Uni Assignments/Section_A/assignment3.py
@@ -7,13 +7,17 @@ def createString(num):
     for i in range(num + 1):
         emptyList.append(str(i))
     listIntoString = ' + '.join(emptyList)
-    answer = eval(listIntoString)
+    answer = 0
+    for x in range(len(listIntoString) + 1):
+        answer += x #maxes out at 1700 numbers roughly
+    
+    #answer = eval(listIntoString) #this works
 
     print(f'{listIntoString} = {answer}')
 
     return answer
 
-for i in range(1, 1000, 10):
+for i in range(1, 10000000):
     createString(i)
 
 endtime = time.time()
