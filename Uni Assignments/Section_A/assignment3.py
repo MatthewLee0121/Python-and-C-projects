2ae7f42a348cd7b4812f792f55435163fccba80d
Matty Laptop <mattylee0121@gmail.com>
2024-11-28 08:07:47 +0000

    lmao

--- Uni Assignments/Section_A/assignment3.py
+++ Uni Assignments/Section_A/assignment3.py
@@ -3,21 +3,24 @@ import time
 starttime = time.time()
 
 def createString(num):
+    print(num)
     emptyList = []
     for i in range(num + 1):
         emptyList.append(str(i))
     listIntoString = ' + '.join(emptyList)
     answer = 0
-    for x in range(len(listIntoString) + 1):
+    for x in range(len(emptyList) + 1):
         answer += x #maxes out at 1700 numbers roughly
-    
-    #answer = eval(listIntoString) #this works
+        if x > 9223372036854775807:
+            print("max safe exceeded")
+            break
+    #answer = eval(listIntoString) #this works  9223372036854775807
 
     print(f'{listIntoString} = {answer}')
 
     return answer
 
-for i in range(1, 10000000):
+for i in range(1, 1000000, 10000):
     createString(i)
 
 endtime = time.time()
